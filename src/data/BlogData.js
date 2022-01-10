aa536b6bce9391b495eb96833f9e04750beada72
nikunjpadasala <49015652+nikunjpadasala@users.noreply.github.com>
2022-01-10 19:57:12 +0500

    Add files via upload

---
+++ src/data/BlogData.js
@@ -0,0 +1,58 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"DALL·E: Creating Images from Text",
+        tags:["machine-learning","neural-network","python"],
+        date:"13 May, 2021",
+        imgSrc:"https://cdn.openai.com/research-covers/dall-e/2x-no-mark.jpg",
+        link:"https://openai.com/blog/dall-e/"
+    },
+    {
+        id:2,
+        name:"CLIP: Connecting Text and Images",
+        tags:["machine-learning","neural-network","python"],
+        date:"5 July, 2021",
+        imgSrc:"https://cdn.openai.com/research-covers/clip/2x-no-mark.jpg",
+        link:"https://openai.com/blog/clip/"
+    },
+    {
+        id:3,
+        name:"Jukebox",
+        tags:["machine-learning","neural-network","python"],
+        date:"6 July, 2021",
+        imgSrc:"https://cdn.openai.com/research-covers/jukebox/2x-no-mark.jpg",
+        link:"https://openai.com/blog/jukebox/"    },
+    {
+        id:4,
+        name:"Solving Rubik’s Cube with a Robot Hand",
+        tags:["machine-learning","neural-network","python"],
+        date:"26 May, 2021",
+        imgSrc:"https://cdn.openai.com/research-covers/solving-rubiks-cube/2x-no-mark.jpg",
+        link:"https://openai.com/blog/solving-rubiks-cube/"    },
+    {
+        id:5,
+        name:"Emergent Tool Use from Multi-Agent Interaction",
+        tags:["machine-learning","neural-network","python"],
+        date:"3 Feb, 2021",
+        imgSrc:"https://cdn.openai.com/research-covers/emergent-tool-use/2x-no-mark.jpg",
+        link:"https://openai.com/blog/emergent-tool-use/"    },
+    {
+        id:6,
+        name:"Learning Dexterity",
+        tags:["machine-learning","neural-network","python"],
+        date:"5 Jan, 2021",
+        imgSrc:"https://cdn.openai.com/research-covers/learning-dexterity/2x-no-mark.jpg",
+        link:"https://openai.com/blog/learning-dexterity/"
+    },
+    {
+        id:7,
+        name:"Ingredients for Robotics Research",
+        tags:["machine-learning","neural-network","python"],
+        date:"26 May, 2021",
+        imgSrc:"https://cdn.openai.com/research-covers/ingredients-for-robotics-research/2x-no-mark.jpg",
+        link:"https://openai.com/blog/ingredients-for-robotics-research/"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,73 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"House-Price-Prediction with python",
+        description:"Prediction house prices are expected to help people who plan to buy a house so they can know the price range in the future, In addition, house price predictions are also beneficial for property investors to know the trend of housing prices.",
+        tags:["machine-learning","neural-network","python"],
+        
+        demo:"https://github.com/nikunjpadasala/Machine-learning",
+        github:"https://github.com/nikunjpadasala/Machine-learning"
+    },
+    {
+        id:2,
+        name:"Malware-Detection using machine learning",
+        description:"AI can't automatically detect every potential malware , but when it combines the modeling of both bad and good behavior, it can be a successful and powerful weapon against even the most advanced malware.",
+        tags:["machine-learning","neural-network","python"],
+        
+        demo:"https://github.com/nikunjpadasala/Machine-learning",
+        github:"https://github.com/nikunjpadasala/Machine-learning"
+        
+    },
+    {
+        id:3,
+        name:"Parkinson dieases detection",
+        description:"Parkinson disease onset detection Using Machine Learning!.",
+        tags:["machine-learning","neural-network","python"],
+        
+        demo:"https://github.com/nikunjpadasala/Machine-learning",
+        github:"https://github.com/nikunjpadasala/Machine-learning"
+    },
+    {
+        id:4,
+        name:"Titanic Dataset Analysis - Classification",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["machine-learning","neural-network","python"],
+        
+        demo:"https://github.com/nikunjpadasala/Machine-learning",
+        github:"https://github.com/nikunjpadasala/Machine-learning"
+       
+    },{
+        id:5,
+        name:"lane_detection with opencv",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["machine-learning","neural-network","python"],
+        
+        demo:"https://github.com/nikunjpadasala/Machine-learning",
+        github:"https://github.com/nikunjpadasala/Machine-learning"
+        
+    },
+    {
+        id:6,
+        name:"butterfly-classification",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["machine-learning","neural-network","python"],
+        
+        demo:"https://github.com/nikunjpadasala/Machine-learning",
+        github:"https://github.com/nikunjpadasala/Machine-learning"
+        
+    },
+    {
+        id:7,
+        name:"GENERATING TEXTS WITH RECURRENT NEURAL NETWORKS IN PYTHON",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["machine-learning","neural-network","python"],
+        
+        demo:"https://github.com/nikunjpadasala/Machine-learning",
+        github:"https://github.com/nikunjpadasala/Machine-learning"
+        
+    },
+    
+  
+] 
